29747c0703a1ee329c4e755c72bd89cdefb52015
nicholas <ndibellohitta1326@sdsu.edu>
2025-05-16 16:22:13 -0600

    some snaps have to be installed as classic, hacked a fix

--- install-snaps.sh
+++ install-snaps.sh
@@ -3,16 +3,29 @@
 SNAPS=(
   "spotify"
   "discord"
-  "code"
-  "obsidian"
-  "nvim"
   "htop"
+)
+
+CLASSIC_SNAPS=(
+  "code" # --classic
+  "obsidian" # --classic
+  "nvim" # --classic
 )
 
 for snap in "${SNAPS[@]}"; do
   if ! snap list | grep -i "$snap" &> /dev/null; then
     echo "Installing Snap: $snap"
     snap install "$snap"
+  else
+    echo "snap already installed: $snap"
+  fi
+done
+
+# TODO: Figure out a more elegant way to refactor this
+for snap in "${CLASSIC_SNAPSSNAPS[@]}"; do
+  if ! snap list | grep -i "$snap" &> /dev/null; then
+    echo "Installing Snap: $snap"
+    snap install "$snap" --classic
   else
     echo "snap already installed: $snap"
   fi
